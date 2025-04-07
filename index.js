381aa08ac3e8b2203e1f9b600aeb511ad7508fa5
Kibiwott <bk.kibiwott@gmail.com>
2025-04-07 20:09:52 +0300

    Added the login auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {PrismaClient} from "@prisma/client"
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const client = new PrismaClient();
 const app = express();
@@ -33,11 +34,58 @@ app.post('/users/acc', async (req, res) => {
 });
 
 app.post('/users/login', async (req, res) => {
-    const { email, password } = req.body;
+    
+    
     try {
+        // Read the email and password from the request body
+        const { email, password } = req.body;
 
+        // Find the user by email
+        const user = await client.user.findFirst({
+            where: {
+                email,
+            },
+            
+        });
+        //check if the user exists
+        if(!user) {
+            
+            res.status(404).json({ message: 'Wrong Email address or password' });
+            return;
+        }
+
+
+
+        // if not, return an error
+
+         // if the user exists, compare the password with the hashed password in the database
+
+        const isMatch=bcrypt.compare(password, user.password,(err,result)=>{
+            
+        // if the password does not match, return an error
+            if(!isMatch){
+                res.status(404).json({ message: 'Wrong Email address or password' });
+                return;
+            } 
+             //if they match,generate token and save it their and sen the address to the user
+             const payload = {
+                id: user.id,
+                email: user.email,
+            };
+            const token =jwt.sign(payload, process.env.JWT_SECRET_KEY);  
+            res.status(200).cookie("authToken", token,{}).json({
+                message: 'Login successful',
+                status:"Success",
+                data: user,
+                
+            });
+
+        })
     }catch (error) {
-        
+        res.status(500).json({ 
+            message: 'Error logging in',
+            status:"Error"
+     });
     }
 });
 
